a0fb95f97ef89990c2f11420fa6c3dbcda8d3b0c
Karthik <mkarthik@me.com>
2019-04-28 12:51:26 -0700

    Add nav shrinking on scroll

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,30 +1,53 @@
-import React from 'react'
-import './styles.css'
+import React, { useState, useEffect } from 'react'
+import './styles.scss'
 
 import Fade from 'react-reveal/Fade'
 import jump from 'jump.js'
+import throttle from 'lodash/throttle'
 
-const Nav = () => (
-  <Fade top duration={400}>
-    <div className="nav">
-      <div className="nav-header">Karthik</div>
-      <div className="nav-items">
-        <span onClick={() => jump('.projects')} className="nav-item left">
-          Projects
-        </span>
-        <span
-          onClick={() => jump('.about')}
-          href="#sectionAbout"
-          className="nav-item"
-        >
-          About
-        </span>
-        <a href="resume.pdf" className="nav-item right">
-          Resume
-        </a>
+const scrollHandler = throttle((setScrollY, scrollY, setCompressNav) => {
+  setScrollY(
+    Math.min(window.scrollY, document.body.offsetHeight - window.innerHeight)
+  )
+  setCompressNav(scrollY > window.scrollY || scrollY <= 0 ? false : true)
+}, 200)
+
+const Nav = () => {
+  const [scrollY, setScrollY] = useState(window.scrollY)
+  const [compressNav, setCompressNav] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () =>
+      scrollHandler(setScrollY, scrollY, setCompressNav)
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [scrollY])
+
+  return (
+    <Fade top duration={400}>
+      <div className={`nav ${compressNav ? 'compressed' : ''}`}>
+        <div className="nav-header">Karthik</div>
+        <div className="nav-items">
+          <span onClick={() => jump('.projects')} className="nav-item">
+            Projects
+          </span>
+          <span onClick={() => jump('.about')} className="nav-item">
+            About
+          </span>
+          <a
+            href={`${process.env.PUBLIC_URL}/resume.pdf`}
+            className="nav-item"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Resume
+          </a>
+        </div>
       </div>
-    </div>
-  </Fade>
-)
+    </Fade>
+  )
+}
 
 export default Nav
