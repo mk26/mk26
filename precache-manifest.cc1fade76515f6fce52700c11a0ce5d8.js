9e1414139932097615679b796af28c1706a4c20c
Karthik <mkarthik@me.com>
2019-04-28 12:53:00 -0700

    Updates

---
+++ precache-manifest.cc1fade76515f6fce52700c11a0ce5d8.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53547f1fefe029b14ffca978ee28762",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd1b293e33c1b983f30e",
+    "url": "/static/css/2.d45e8b06.chunk.css"
+  },
+  {
+    "revision": "58a9783ef6319dc33714",
+    "url": "/static/css/main.364a3f40.chunk.css"
+  },
+  {
+    "revision": "dd1b293e33c1b983f30e",
+    "url": "/static/js/2.509e289d.chunk.js"
+  },
+  {
+    "revision": "58a9783ef6319dc33714",
+    "url": "/static/js/main.67673d0b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "851eba399a6f34043ce406255f5e1742",
+    "url": "/static/media/about-bg.851eba39.jpg"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "/static/media/default-skin.b257fa9c.svg"
+  },
+  {
+    "revision": "4244fba9747539c5b7b469b80311cf41",
+    "url": "/static/media/header-bg.4244fba9.jpg"
+  },
+  {
+    "revision": "55ae1ae80671ed3cb438399864493f8f",
+    "url": "/static/media/lelivre-books.55ae1ae8.png"
+  },
+  {
+    "revision": "75a4ec117f983ac892c6bb6b837cf58b",
+    "url": "/static/media/lelivre-books_thumb.75a4ec11.jpg"
+  },
+  {
+    "revision": "ad32276a00f5f0393a0949b03203f90b",
+    "url": "/static/media/lelivre-home.ad32276a.png"
+  },
+  {
+    "revision": "ca547b6686f29ddf76136c3c45ac3066",
+    "url": "/static/media/lelivre-home_thumb.ca547b66.jpg"
+  },
+  {
+    "revision": "971219ccd44c4ea80aa1ea57b1fbb91b",
+    "url": "/static/media/lelivre-search.971219cc.png"
+  },
+  {
+    "revision": "5d0ca65aef59233bcec0a8c20d7fee42",
+    "url": "/static/media/lelivre-search_thumb.5d0ca65a.jpg"
+  },
+  {
+    "revision": "4eb6c239e63859a20e3fd20504e6b856",
+    "url": "/static/media/lelivre.4eb6c239.png"
+  },
+  {
+    "revision": "b9b45c0f818290a39633e4b04cc6f4a3",
+    "url": "/static/media/mk.b9b45c0f.jpg"
+  },
+  {
+    "revision": "9c0c965921fe591a4f0e95e70073754f",
+    "url": "/static/media/projects-bg.9c0c9659.jpg"
+  },
+  {
+    "revision": "e77e0cbd4d490b0078d1bb059e89983c",
+    "url": "/static/media/shipwars-game1.e77e0cbd.png"
+  },
+  {
+    "revision": "6b86074de2fe17358c8d2e46f87b9f09",
+    "url": "/static/media/shipwars-game1_thumb.6b86074d.jpg"
+  },
+  {
+    "revision": "65dcc61d9c0866d73d84fc32c55fcf11",
+    "url": "/static/media/shipwars-game2.65dcc61d.png"
+  },
+  {
+    "revision": "144e52eef4b274853a71df617889520d",
+    "url": "/static/media/shipwars-game2_thumb.144e52ee.jpg"
+  },
+  {
+    "revision": "5508f5d5912f084e8affa4f572b06adb",
+    "url": "/static/media/shipwars-game3.5508f5d5.png"
+  },
+  {
+    "revision": "421ded21db76464ff36e69d43e465053",
+    "url": "/static/media/shipwars-game3_thumb.421ded21.jpg"
+  },
+  {
+    "revision": "90ba9f4d74a34e59a01261b007ce6754",
+    "url": "/static/media/shipwars-intro.90ba9f4d.png"
+  },
+  {
+    "revision": "debee1c510077f95a0d16c3264f3a99a",
+    "url": "/static/media/shipwars-intro_thumb.debee1c5.jpg"
+  },
+  {
+    "revision": "4d115b13c8c051a4cd6cac58d3a14d6d",
+    "url": "/static/media/shipwars-stats.4d115b13.png"
+  },
+  {
+    "revision": "cdc70ad13787ac612e918d3daa52862d",
+    "url": "/static/media/shipwars-stats_thumb.cdc70ad1.jpg"
+  },
+  {
+    "revision": "eacaa6fdc9b0b6ac47a99410c946fbf8",
+    "url": "/static/media/shipwars.eacaa6fd.png"
+  },
+  {
+    "revision": "fcff35d104fb594a1c0371e3893c690f",
+    "url": "/static/media/spark-list.fcff35d1.png"
+  },
+  {
+    "revision": "7548043f7c59bff37d39d7ae27c57c76",
+    "url": "/static/media/spark-map.7548043f.png"
+  },
+  {
+    "revision": "54d2daa098f727531d25cd88f4eef5cd",
+    "url": "/static/media/spark-map_thumb.54d2daa0.jpg"
+  },
+  {
+    "revision": "da53600bf50a8760bab21109ff021342",
+    "url": "/static/media/spark-msg.da53600b.png"
+  },
+  {
+    "revision": "52c993f842189c7a8d43108483d6be51",
+    "url": "/static/media/spark-notification.52c993f8.png"
+  },
+  {
+    "revision": "f52a7b2d34de5c737b016efaa91d226d",
+    "url": "/static/media/spark-passcode.f52a7b2d.png"
+  },
+  {
+    "revision": "af7f9ff4dda5e5302cf32b41b4591ba7",
+    "url": "/static/media/spark-settings.af7f9ff4.png"
+  },
+  {
+    "revision": "d42304306e4a71fb089dd70e5f6d752d",
+    "url": "/static/media/spark.d4230430.png"
+  },
+  {
+    "revision": "c68d5230f1fd8c88932f051a50e5cf8e",
+    "url": "/static/media/stockr-home.c68d5230.png"
+  },
+  {
+    "revision": "68f83dcc64e581b0e683b287a186805f",
+    "url": "/static/media/stockr-prod.68f83dcc.png"
+  },
+  {
+    "revision": "f6e798f7a8f174b23ddafdc92ee90ff8",
+    "url": "/static/media/stockr-report.f6e798f7.png"
+  },
+  {
+    "revision": "91ba63688c77084b88b19ae381f0170d",
+    "url": "/static/media/stockr.91ba6368.png"
+  },
+  {
+    "revision": "20b05a7dc2f314ab084b6557212b01dd",
+    "url": "/static/media/task-notes.20b05a7d.png"
+  },
+  {
+    "revision": "c41532a277662b1e2bf644338712b4d6",
+    "url": "/static/media/teams-all-teams.c41532a2.png"
+  },
+  {
+    "revision": "1b495a0c79dd6f053c07a38355f4c5e0",
+    "url": "/static/media/teams-channel.1b495a0c.png"
+  },
+  {
+    "revision": "94dd03b435a6316c0d84db63c1790df8",
+    "url": "/static/media/teams-conv.94dd03b4.png"
+  },
+  {
+    "revision": "dbe929472ec73d25b31cd6cb7a5dbecc",
+    "url": "/static/media/teams-create-task.dbe92947.png"
+  },
+  {
+    "revision": "328f1df5e3f7390e3a407c0bdb0bb104",
+    "url": "/static/media/teams-create-team.328f1df5.png"
+  },
+  {
+    "revision": "f4053f0a120d9a5db0efb783c5abe945",
+    "url": "/static/media/teams-files-list.f4053f0a.png"
+  },
+  {
+    "revision": "02da346bb8997d690326a8bdd9b4f925",
+    "url": "/static/media/teams-notifications.02da346b.png"
+  },
+  {
+    "revision": "6a60ba7d7b0ca54d0b039352a32c77b2",
+    "url": "/static/media/teams-team-info.6a60ba7d.png"
+  },
+  {
+    "revision": "62e8233f8749bc49b41a83915565490d",
+    "url": "/static/media/teams.62e8233f.png"
+  },
+  {
+    "revision": "62d0d315779301886bc1b4a93656a52f",
+    "url": "/static/media/tetris-game.62d0d315.png"
+  },
+  {
+    "revision": "4e0b7e78376447a9edcc7a627efc76b6",
+    "url": "/static/media/tetris-gameover.4e0b7e78.png"
+  },
+  {
+    "revision": "1c389040efaace6d58b6d5322c4d276c",
+    "url": "/static/media/tetris-paused.1c389040.png"
+  },
+  {
+    "revision": "16e94fe00b08240d88f2d17ccac74cf5",
+    "url": "/static/media/tetris.16e94fe0.png"
+  },
+  {
+    "revision": "c0cfb5450c4af14d59438c7eaf4372ad",
+    "url": "/static/media/zones-add.c0cfb545.png"
+  },
+  {
+    "revision": "dd43709598c79620c293e12535dfe7e8",
+    "url": "/static/media/zones-add_thumb.dd437095.jpg"
+  },
+  {
+    "revision": "1ed5731a2b2079303715e5dfc8da1bc9",
+    "url": "/static/media/zones-list.1ed5731a.png"
+  },
+  {
+    "revision": "7497bd9c683cbe61bb75760dee152991",
+    "url": "/static/media/zones-list_thumb.7497bd9c.jpg"
+  },
+  {
+    "revision": "b1802d434407f9aa217ab507069ae07d",
+    "url": "/static/media/zones-map.b1802d43.png"
+  },
+  {
+    "revision": "95ec00d49e7dd0c0dbf32d7ec3076ed3",
+    "url": "/static/media/zones-map_thumb.95ec00d4.jpg"
+  },
+  {
+    "revision": "69dfbda311a37335ec46a93f0f47729b",
+    "url": "/static/media/zones-reminder.69dfbda3.png"
+  },
+  {
+    "revision": "38466dfa08722c2e405b6175e512d047",
+    "url": "/static/media/zones.38466dfa.png"
+  }
+]);
