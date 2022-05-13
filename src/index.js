0ee448ad711f3d05538777fe6e2424f4112fede2
Karthik <mkarthik@me.com>
2022-05-12 23:12:15 -0700

    Update to React 18 style create root

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 
 import App from './App'
 
@@ -9,10 +9,12 @@ import { ParallaxProvider } from 'react-scroll-parallax'
 import 'react-photoswipe/lib/photoswipe.css'
 import './index.scss'
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container)
+
+root.render(
   <ParallaxProvider>
     <App />
-  </ParallaxProvider>,
-  document.getElementById('root')
+  </ParallaxProvider>
 )
 registerServiceWorker()
